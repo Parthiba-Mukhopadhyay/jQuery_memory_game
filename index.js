e64304a5b61ad2eb9f5d26d237857c5e870077ad
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-09-11 00:06:29 +0530

    update

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var buttonColours=["red", "blue", "green", "yellow"];
 var gamePattern=[];
+var userClickedPattern=[];
+
 $(".btn").click(function(){
-    alert("The paragraph was clicked.");
+    var userChosenColour=$(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
   });
+
 function nextSequence()
 {
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio=Audio("/sounds"+randomChosenColour+".mp3");
-    audio.play();
+    playSound(randomChosenColour);
 }
+
+function playSound(name){
+  var audio=Audio("/sounds"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(()=>{ $("#"+currentColour).removeClass("pressed");
+},100);
+}
+
