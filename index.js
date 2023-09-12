8980c21fa4651a0b644e3c5a44c90503510a92d5
Parthiba-Mukhopadhyay <meparthiba@gmail.com>
2023-09-12 12:57:25 +0530

    final_edits

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
 var userClickedPattern = [];
-
 var started = false;
 var level = 0;
 
 $(".btn").click(function() {
-
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
   checkAnswer(userClickedPattern.length-1);
 });
 
@@ -27,48 +22,35 @@ $(document).keypress(function() {
   }
 });
 
-
-
-
 function checkAnswer(currentLevel) {
-
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
       console.log("success");
-
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           nextSequence();
         }, 1000);
       }
-
     } else {
-
       console.log("wrong");
-
       playSound("wrong");
-
+      $("body").removeClass("initial")
       $("body").addClass("wrong");
       setTimeout(function () {
         $("body").removeClass("wrong");
+        $("body").addClass("initial");
       }, 200);
-
       $("#title_head").text("Game Over, Press Any Key to Restart");
       startOver();
     }
-
 }
 
 function nextSequence() {
-
   userClickedPattern = [];
   level++;
   $("#title_head").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
@@ -85,10 +67,7 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-//1. Create a new function called startOver().
 function startOver() {
-
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
   level = 0;
   gamePattern = [];
   started = false;
